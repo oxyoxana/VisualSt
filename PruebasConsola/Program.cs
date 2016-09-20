cd1837335aa441410553ad12e0def8b494114bba
Oxana <oxana@PC-05>
2016-09-20 09:42:40 +0200

    Agregar archivos de proyecto.

---
+++ PruebasConsola/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un importe: \n");
+            double importe = 0;
+            string mensaje = "";
+            importe = double.Parse(Console.ReadLine());
+            mensaje = importe > (99.9) ? "No se dispone de ese importe"
+                : "Se retirará " + importe;
+            Console.WriteLine(mensaje);
+            Console.ReadKey();
+        }
+    }
+}
